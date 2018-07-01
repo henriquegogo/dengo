7fa70942622d0fa3cad012fb92b6d012d782302f
Henrique Gogó <henriquegogo@gmail.com>
2018-07-01 17:37:24 -0300

    Adding LXTerminal config files

--- installer.sh
+++ installer.sh
@@ -7,11 +7,13 @@ mkdir ~/.config/openbox
 mkdir ~/.config/tint2
 mkdir ~/.config/synapse
 mkdir ~/.config/gtk-3.0
+mkdir ~/.config/lxterminal
 
 cp ./openbox/* ~/.config/openbox/
 cp ./tint2/* ~/.config/tint2/
 cp ./synapse/* ~/.config/synapse/
 cp ./gtk-3.0/* ~/.config/gtk-3.0/
+cp ./lxterminal/* ~/.config/lxterminal/
 
 sudo apt install git -y
 git clone https://github.com/vinceliuice/vimix-icon-theme --depth=1
