c218f92b01f47d765aa57d253dbe9039770415ef
Henrique Gogó <henriquegogo@gmail.com>
2018-07-01 17:31:29 -0300

    Removing thunar from default installation

---
+++ installer-thunar.sh
@@ -0,0 +1,2 @@
+sudo apt install thunar-archive-plugin thunar-gtkhash thunar-media-tags-plugin thunar-volman thunar-vcs-plugin tumbler-plugins-extra -y
+sudo apt install ffmpegthumbnailer ffmpegthumbs -y
--- installer.sh
+++ installer.sh
@@ -1,8 +1,6 @@
-sudo apt install xinit openbox xcompmgr tint2 feh volti
-sudo apt install thunar-archive-plugin thunar-gtkhash thunar-media-tags-plugin thunar-volman thunar-vcs-plugin tumbler-plugins-extra
-sudo apt install ffmpegthumbnailer ffmpegthumbs
-sudo apt install lxappearance lxterminal rofi synapse
-sudo apt install dmz-cursor-theme numix-gtk-theme fonts-cantarell fonts-noto
+sudo apt install xinit openbox xcompmgr tint2 feh volti -y
+sudo apt install lxappearance lxterminal rofi synapse -y
+sudo apt install dmz-cursor-theme numix-gtk-theme fonts-cantarell fonts-noto -y
 
 mkdir ~/.config
 mkdir ~/.config/openbox
@@ -15,7 +13,7 @@ cp ./tint2/* ~/.config/tint2/
 cp ./synapse/* ~/.config/synapse/
 cp ./gtk-3.0/* ~/.config/gtk-3.0/
 
-sudo apt install git
+sudo apt install git -y
 git clone https://github.com/vinceliuice/vimix-icon-theme --depth=1
 cd vimix-icon-theme
 ./Installer.sh
