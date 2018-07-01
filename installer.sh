e8227da3e3016846878738287c96f751c3e5c4d5
Henrique Gogó <henriquegogo@gmail.com>
2018-07-01 17:52:50 -0300

    Installing pcmanfm instead of thunar

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 sudo apt install xinit openbox xcompmgr tint2 feh volti -y
-sudo apt install lxappearance lxterminal rofi synapse -y
+sudo apt install lxappearance lxterminal pcmanfm rofi synapse -y
 sudo apt install dmz-cursor-theme numix-gtk-theme fonts-cantarell fonts-noto -y
 
 mkdir ~/.config
