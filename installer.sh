84715fad3397d14923b4eb86aa2fc8276fda0815
Henrique Gogó <henriquegogo@gmail.com>
2018-07-01 15:32:17 -0300

    Adding config files for synapse launcher and gtk

--- installer.sh
+++ installer.sh
@@ -6,16 +6,15 @@ sudo apt install dmz-cursor-theme numix-gtk-theme fonts-cantarell fonts-noto
 
 mkdir ~/.config/openbox
 mkdir ~/.config/tint2
+mkdir ~/.config/synapse
+mkdir ~/.config/gtk-3.0
 
 cp ./openbox/* ~/.config/openbox/
 cp ./tint2/* ~/.config/tint2/
+cp ./synapse/* ~/.config/synapse/
+cp ./gtk-3.0/* ~/.config/gtk-3.0/
 
 sudo apt install git
 git clone https://github.com/vinceliuice/vimix-icon-theme --depth=1
 cd vimix-icon-theme
 ./Installer.sh
-
-echo "##########################################"
-echo "## Run 'lxappearance' and select        ##"
-echo "## Numix + DMZ White + Vimix Paper Icon ##"
-echo "##########################################"
