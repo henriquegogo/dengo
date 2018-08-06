9fac16b1c14cb8e9536216845e807112c3e39cd3
Henrique Soares <henrique.vasconcelos@hp.com>
2018-08-06 08:57:45 -0300

    Changing openbox to customized dwm

--- installer.sh
+++ installer.sh
@@ -5,25 +5,27 @@ echo "#####################"
 echo; echo ">>> Updating packages list"
 sudo apt update
 echo; echo ">>> Installing essential environment packages"
-sudo apt install xinit dbus openbox tint2 feh volti wicd -y
-echo; echo ">>> Installing usability applications"
-sudo apt install lxterminal pcmanfm dmenu -y
+sudo apt install xinit dbus dwm dmenu wmctrl feh wicd scrot -y
 echo; echo ">>> Installing fonts and icons"
-sudo apt install dmz-cursor-theme numix-gtk-theme fonts-cantarell fonts-noto-hinted fonts-noto-mono -y
+sudo apt install numix-gtk-theme fonts-cantarell fonts-noto-hinted fonts-noto-mono -y
+
+echo; echo ">>> Clonning dwm from repository"
+git clone git://git.suckless.org/dwm
+echo; echo ">>> Coping dwm config files and building"
+cp ./dwm-config.h dwm/config.h
+cd dwm
+git checkout 6.1
+make
+sudo make install
+cd ..
 
 echo; echo ">>> Creating folders"
 mkdir -p ~/.icons
-mkdir -p ~/.config/openbox
-mkdir -p ~/.config/tint2
-mkdir -p ~/.config/lxterminal
 mkdir -p ~/.config/gtk-3.0
 mkdir -p ~/Pictures
 echo "OK!"
 
 echo; echo ">>> Coping files"
-cp ./openbox/* ~/.config/openbox/
-cp ./tint2/* ~/.config/tint2/
-cp ./lxterminal/* ~/.config/lxterminal/
 cp ./gtk-3.0/* ~/.config/gtk-3.0/
 cp ./_gtkrc-2.0 ~/.gtkrc-2.0
 cp ./sunset.jpg ~/Pictures/
