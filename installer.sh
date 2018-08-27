3fbf91372580d85609b435eab572b6ed7377d732
Henrique Soares <henrique.vasconcelos@hp.com>
2018-08-27 13:50:57 -0300

    Using evilwm instead of dwm

--- installer.sh
+++ installer.sh
@@ -4,23 +4,10 @@ echo "#####################"
 
 echo; echo ">>> Updating packages list"
 sudo apt update
-echo; echo ">>> Installing builder packages"
-sudo apt install build-essential make libx11-dev libxft-dev libxinerama-dev -y
 echo; echo ">>> Installing essential environment packages"
-sudo apt install xinit dbus dwm dmenu rofi wmctrl feh wicd scrot -y
+sudo apt install xinit dbus dwm dmenu wmctrl xbindkeys feh wicd scrot wget unzip -y
 echo; echo ">>> Installing fonts and icons"
-sudo apt install numix-gtk-theme fonts-cantarell fonts-noto-hinted fonts-noto-mono -y
-
-echo; echo ">>> Clonning dwm from repository"
-git clone git://git.suckless.org/dwm
-echo; echo ">>> Coping dwm config files and building"
-cd dwm
-git checkout 6.1
-cp ../dwm-config.h config.h
-patch --ignore-whitespace dwm.c ../preventMouseFocus.patch
-make
-sudo make install
-cd ..
+sudo apt install numix-gtk-theme fonts-cantarell fonts-noto-hinted fonts-noto-mono xfonts-75dpi xfonts-100dpi xfonts-terminus -y
 
 echo; echo ">>> Creating folders"
 mkdir -p ~/.icons
@@ -32,11 +19,13 @@ echo; echo ">>> Coping files"
 cp ./gtk-3.0/* ~/.config/gtk-3.0/
 cp ./_gtkrc-2.0 ~/.gtkrc-2.0
 cp ./sunset.jpg ~/Pictures/
+cp ./evilwmrc ~/.evilwmrc
+cp ./xbindkeysrc ~/.xbindkeysrc
 echo "OK!"
 
-echo; echo ">>> Installing git"
-sudo apt install git -y
-echo; echo ">>> Clonning Vimix icon theme"
-git clone https://github.com/vinceliuice/vimix-icon-theme --depth=1
+echo; echo ">>> Downloading Vimix icon theme"
+wget https://github.com/vinceliuice/vimix-icon-theme/archive/master.zip -O vimix.zip
+echo; echo ">>> Unzip Vimix icon theme"
+unzip vimix.zip
 echo; echo ">>> Coping Vimix icon theme"
-cp -r vimix-icon-theme/Paper-Vimix ~/.icons
+cp -r vimix-icon-theme-master ~/.icons
