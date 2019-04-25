f13d7838c92ed05711ebefb100ff43ab1a94b783
Henrique Gogó <henriquegogo@gmail.com>
2019-04-25 01:18:01 -0300

    Changing icons set for someone from repository

--- installer.sh
+++ installer.sh
@@ -8,12 +8,11 @@ sudo apt install xinit x11-xserver-utils dbus dmenu xbindkeys xcompmgr feh scrot
 echo; echo ">>> Installing window manager"
 sudo apt install evilwm fluxbox -y
 echo; echo ">>> Installing fonts and icons"
-sudo apt install numix-gtk-theme dmz-cursor-theme fonts-symbola fonts-cantarell fonts-noto-hinted fonts-noto-mono xfonts-75dpi xfonts-100dpi xfonts-terminus -y
+sudo apt install numix-gtk-theme papirus-icon-theme dmz-cursor-theme fonts-symbola fonts-cantarell fonts-noto-hinted fonts-noto-mono xfonts-75dpi xfonts-100dpi xfonts-terminus -y
 echo; echo ">>> Installing applications"
 sudo apt install lxterminal pcmanfm wicd xarchiver galculator -y
 
 echo; echo ">>> Creating folders"
-mkdir -p ~/.icons
 mkdir -p ~/.config
 mkdir -p ~/.fluxbox
 mkdir -p ~/Pictures
@@ -27,12 +26,3 @@ cp -r ./Pictures/* ~/Pictures
 cp -r ./_config/* ~/.config
 cp -r ./_fluxbox/* ~/.fluxbox
 echo "OK!"
-
-echo; echo ">>> Downloading Vimix icon theme"
-wget https://github.com/vinceliuice/vimix-icon-theme/archive/master.zip -O vimix.zip
-echo; echo ">>> Unzip Vimix icon theme"
-unzip vimix.zip > /dev/null
-echo; echo ">>> Coping Vimix icon theme"
-cp -r vimix-icon-theme-master/Paper-Vimix ~/.icons
-echo; echo ">>> Removing Vimix installer files"
-rm vimix* -rf
