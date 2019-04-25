8644bafcf0b012368c1475a7a8bfcfe0ce2b36a2
Henrique Gogó <henriquegogo@gmail.com>
2019-04-25 01:25:21 -0300

    Removing fluxbox

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ sudo apt update
 echo; echo ">>> Installing essential environment packages"
 sudo apt install xinit x11-xserver-utils dbus dmenu xbindkeys xcompmgr feh scrot wget unzip -y
 echo; echo ">>> Installing window manager"
-sudo apt install evilwm fluxbox -y
+sudo apt install evilwm -y
 echo; echo ">>> Installing fonts and icons"
 sudo apt install numix-gtk-theme papirus-icon-theme dmz-cursor-theme fonts-symbola fonts-cantarell fonts-noto-hinted fonts-noto-mono xfonts-75dpi xfonts-100dpi xfonts-terminus -y
 echo; echo ">>> Installing applications"
@@ -14,7 +14,6 @@ sudo apt install lxterminal pcmanfm wicd xarchiver galculator -y
 
 echo; echo ">>> Creating folders"
 mkdir -p ~/.config
-mkdir -p ~/.fluxbox
 mkdir -p ~/Pictures
 echo "OK!"
 
@@ -24,5 +23,4 @@ cp ./_xbindkeysrc ~/.xbindkeysrc
 cp ./_evilwmrc ~/.evilwmrc
 cp -r ./Pictures/* ~/Pictures
 cp -r ./_config/* ~/.config
-cp -r ./_fluxbox/* ~/.fluxbox
 echo "OK!"
